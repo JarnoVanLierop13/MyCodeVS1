783a255ba7cb4ee32901008aaa3e2635358b83a1
Jarno van Lieorp <24877@ma-web.nl>
2018-12-20 21:27:13 +0100

    My scripts for vertical slice 1

---
+++ HealthPlayer.cs
@@ -0,0 +1,66 @@
+﻿//Add to player
+//--------------------
+//By Jarno van Lierop
+//GD2A
+//--------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HealthPlayer : MonoBehaviour {
+
+    private int playerStartingHealth = 100;
+    private int playerCurrentHealth;
+    private int tempEnemyDamage = 1;
+    public bool playerDeath;
+
+    public Transform Enemy;
+    public Slider healthBar;
+    public GameObject GameOverMenu;
+
+    private void Awake()
+    {
+        GameOverMenu.gameObject.SetActive(false);
+        playerCurrentHealth = playerStartingHealth;
+        healthBar.maxValue = playerCurrentHealth;
+        healthBar.minValue = 0;
+        playerDeath = false;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            TakeDamage();
+        }
+    }
+
+    void TakeDamage()
+    {
+        playerCurrentHealth -= tempEnemyDamage;
+        healthBar.value = playerCurrentHealth;
+    }
+
+    void Update() {
+        healthBar.value = playerCurrentHealth;
+        if (playerCurrentHealth <= 0)
+        {
+            PlayerDeath();
+        }
+
+        if (playerDeath == true && Input.GetKey(KeyCode.R)) {
+            SceneManager.LoadScene("Jarno", LoadSceneMode.Single);//reload scene
+        }
+	}
+
+    void PlayerDeath()
+    { 
+        playerDeath = true;
+        Time.timeScale = 0.0f;//freeze
+        GameOverMenu.gameObject.SetActive(true);
+    }
+  
+  
+}
---
+++ LockOn.cs
@@ -0,0 +1,60 @@
+﻿//Add to player
+//Add to camera
+//--------------------
+//By Jarno van Lierop
+//GD2A
+//--------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockOn : MonoBehaviour {
+
+    private string enemyTag = "";
+    public GameObject focusBar;
+    public GameObject focusMarker;
+    private bool lockedOn;//lockedon: yes or no
+    public Transform enemyTarget;
+    public Transform playerTarget;
+
+    void LockOnTarget()
+    {
+        lockedOn = true;
+        enemyTag = "Enemy";//set lockon target Enemy
+        transform.LookAt(enemyTarget);//focus camera on Enemy
+    }
+    void ReleaseTarget()
+    {
+        lockedOn = false;
+        enemyTag = "";//remove Enemy as target
+        transform.LookAt(playerTarget);//focus camera on Player
+    }
+
+    void Start()
+    { //all begin as false
+        lockedOn = false;
+        focusBar.gameObject.SetActive(false);
+        focusMarker.gameObject.SetActive(false);
+    }
+
+    void Update () {
+        if (lockedOn == false)
+        {
+            ReleaseTarget();
+            if (Input.GetKeyUp(KeyCode.L))
+            {
+                LockOnTarget();//call
+                focusBar.gameObject.SetActive(true);
+                focusMarker.gameObject.SetActive(true);
+            }
+        } else {
+            LockOnTarget();
+            if (Input.GetKeyUp(KeyCode.L))
+            {
+                ReleaseTarget();//call
+                focusBar.gameObject.SetActive(false);
+                focusMarker.gameObject.SetActive(false);
+            }
+        }            
+	}   
+}
